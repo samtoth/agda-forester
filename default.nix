be369eb7286e1fcc887f03d4c3bc96d2fdfc9778
Samuel Toth <psyst8@nottingham.ac.uk>
2025-05-27 14:44:32 +0100

    Findlib mirror

--- default.nix
+++ default.nix
@@ -8,17 +8,7 @@ let
   pkgs = import ./nix/nixpkgs.nix { inherit system; };
   forester = builtins.getFlake "sourcehut:~jonsterling/ocaml-forester?rev=56de06afe952d752c1a13fdcd8bb56c5fef9956f";
 
-  myForester = forester.packages.${system}.default //
-               {
-                 overrideAttributes = old: {
-                   ocamlPackages = old.ocamlPackages.findlib.overrideAttrs (old': {
-                     src = pkgs.fetchurl {
-                       url = "http://download2.camlcity.org/download/findlib${old'.version}.tar.gz";
-                       sha256 = "0ci6nps2qgkhfjqji18qjc26rid9gkpmxzlb1svg5wwair0qvb0s";
-                     };
-                   });
-                 };
-               };
+  myForester = forester.packages.${system}.default;
 
   hsPkgs = pkgs.ourHaskellPackages;
   agdaForester = hsPkgs.callCabal2nix "agda-forester" ./. {} //
--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,9 +1,25 @@
-args: import (builtins.fetchTarball {
+args:
+# We have to temporarily override ocamlPackages - Maybe findlib mirror will come back up
+# or if not, will give a better solution to this.
+let ocamlOverlay = pkgs: super: {
+    ocamlPackages = pkgs.ocamlPackages.overrideScope' (oself: osuper: {
+    findlib = osuper.findlib.overrideAttrs (old: {
+      version = "1.9.6";
+      src = pkgs.fetchurl {
+        url = "http://download2.camlcity.org/download/findlib-1.9.6.tar.gz";
+        sha256 = "0ci6nps2qgkhfjqji18qjc26rid9gkpmxzlb1svg5wwair0qvb0s";
+      };
+    });
+    });
+    };
+in
+import (builtins.fetchTarball {
   name   = "nixpkgs";
   url    = "https://github.com/nixos/nixpkgs/archive/11cb3517b3af6af300dd6c055aeda73c9bf52c48.tar.gz";
   sha256 = "sha256:1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
 }) ({
   overlays = [
     (import ./haskell-packages.nix)
+    (ocamlOverlay)
     ];
 } // args)
